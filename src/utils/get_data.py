f31382f5febdf485f166c9455ea9fdc4a5061fdb
Jiden Ben Fadhel <jiden.benfadhel@edu.esiee.fr>
2025-12-15 00:37:18 +0100

    get_data.py pour ajouter le jeu de donnée au format brut

---
+++ src/utils/get_data.py
@@ -0,0 +1,18 @@
+import zipfile
+import pandas as pd
+import json
+import requests
+import os
+
+api_url = "https://api.insee.fr/melodi/file/DS_ETAT_CIVIL_NAIS_COMMUNES/DS_ETAT_CIVIL_NAIS_COMMUNES_CSV_FR"
+destination = "data/raw"
+zip_path = os.path.join(destination, "DS_ETAT_CIVIL_NAIS_COMMUNES.zip")
+
+response = requests.get(api_url, verify= False)
+response.raise_for_status()
+
+with open(zip_path, "wb") as f:
+    f.write(response.content)
+
+with zipfile.ZipFile(zip_path, "r") as zip_ref:
+    zip_ref.extractall(destination)
